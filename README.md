ca918aad01c69cff1fc29c3cd3619abb833e5311
Aymeric <Aymkdn@users.noreply.github.com>
2020-05-02 15:17:05 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # tuyacloud-php
-PHP Library to access to the SmartLife / Tuya objects using the Cloud of Home Assistant
+
+PHP Library to access to the SmartLife / Tuya objects using the Cloud of Home Assistant.
+
+It's based on https://github.com/unparagoned/cloudtuya
+
+## Usage
+
+```php
+<?php
+require 'TuyaCloud.php';
+
+$tuya = new TuyaCloud([
+  "userName" => "email@domain.com", // username/email to access to SmartLife/Tuya app
+  "password" => "MyPassword", // password to access to SmartLife/Tuya app
+  "bizType" => "smart_life", // type ('tuya' or 'smart_life')
+  "countryCode" => "33", // Country code (International dialing number), e.g. "33" for France or "1" for USA
+  "region" => "eu" // region (az=Americas, ay=Asia, eu=Europe)
+]);
+
+// to get a list of your devices
+$devices = $tuya->getDevices();
+foreach($devices as $device) {
+  echo "Name: ".$device->name."<br>";
+  echo "ID: ".$device->id."<br>";
+  echo "Type: ".$device->dev_type."<br>";
+  if ($device->dev_type !== "scene") {
+    echo "State: ".$device->data->state."<br>";
+    echo "Online: ".$device->data->online."<br>";
+  }
+}
+
+// to switch on a device called "switch 1"
+$tuya->setState([
+  "name" => "switch 1",
+  "value" => "on"
+]);
+
+// to switch off a device with ID "123456"
+$tuya->setState([
+  "id" => "123456",
+  "value" => "off"
+]);
+
+// to stop a cover
+$tuya->setState([
+  "name" => "cover living room",
+  "command" => "startStop",
+  "value" => 0
+]);
+
+// to get the state of a device
+echo "State Swtich 1 => ".$tuya->setState([
+  "name" => "switch 1"
+]);
+?>
+```
---
+++ TuyaCloud.php
@@ -0,0 +1,163 @@
+<?php
+class TuyaCloud {
+  var $profile = [];
+  var $uri = '';
+  var $tokens = '';
+  var $accessToken = '';
+
+  /**
+   * Init the class with the profile information
+   *
+   * @param {Array} $profile
+   *   @param {String} $userName The email used as username to access to SmartLife/Tuya app
+   *   @param {String} $password The related password to access to SmartLife/Tuya app
+   *   @param {String} $bizType Two possible values 'smart_life' or 'tuya'
+   *   @param {String} $countryCode Country code (International dialing number), e.g. "33" for France or "1" for USA
+   *   @param {String} $region The region code: "az" for Americas, "ay" for Asia, "eu" for Europe
+   */
+  function TuyaCloud($profile) {
+    $this->profile = $profile;
+    $this->profile['from'] = 'tuya';
+    $this->uri = 'https://px1.tuya'.$profile["region"].'.com/homeassistant';
+    // log in
+    $this->login();
+  }
+
+  function post($uri, $body) {
+    $ch = curl_init($uri);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json'
+    ));
+    $result = curl_exec($ch);
+    return json_decode($result);
+  }
+
+  /**
+   * It will login and get the tokens
+   */
+  function login() {
+    $ch = curl_init($this->uri."/auth.do");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($this->profile));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/x-www-form-urlencoded'
+    ));
+
+    $data = curl_exec($ch);
+    $this->tokens = json_decode($data);
+    $this->accessToken = $this->tokens->access_token;
+  }
+
+  /**
+   * Retrieve all the devices and scenario associated with the user
+   *
+   * @return {Array} Array of devices, e.g. [ {"data": {"online": true, "state": false}, "name": "Switch 1", "icon": "https://images.tuyaeu.com/smart/product_icon2/cz_1.png", "id": "4194aef6cb", "dev_type": "switch", "ha_type": "switch" }, …]
+   */
+  function getDevices() {
+    // Scan network otherwise or no device id in options
+    $body = [
+      "header" => [
+        "name" => 'Discovery',
+        "namespace" => 'discovery',
+        "payloadVersion" => 1
+      ],
+      "payload" => [
+        "accessToken" => $this->accessToken
+      ]
+    ];
+    $json = $this->post($this->uri.'/skill', $body);
+    return $json->payload->devices;
+  }
+
+  /**
+   * Set a state for a device
+   *
+   * @param {Object} $options
+   *   @param {String} [$id] The device ID
+   *   @param {String} [$name] The device Name: either the ID or the Name of the device must be provided
+   *   @param {String} [$command="turnOnOff"] The command related to the value (see below)
+   *   @param {Various} $value The value to apply (e.g. 1 to turn on, 0 to turn off)
+   *
+   * @return {Boolean} return TRUE if the operation was a success
+   *
+   * @example
+   * The commands are (based on https://github.com/PaulAnnekov/tuyaha/blob/master/tuyaha/devices/):
+   *   - All Devices:
+   *     -> "turnOnOff" (value 0 === Off, value 1 === On)
+   *   - Cover:
+   *     -> "startStop" (value 0) to stop a cover
+   *   - Climate:
+   *     -> "temperatureSet"
+   *     -> "windSpeedSet" (value is fan_mode)
+   *     -> "modeSet" (value is operation_mode)
+   *   - Fan:
+   *     -> "windSpeedSet" (value is fan_mode)
+   *   - Light:
+   *     -> "brightnessSet"
+   *     -> "colorSet" (value is hsv_color)
+   *     -> "colorTemperatureSet" (value is color_temp)
+   */
+  function setState($options) {
+    // by default we want to turn on or off
+    if (!isset($options['command'])) $options['command']='turnOnOff';
+    // transform "on" and true to 1
+    // transform "off" and false to 0
+    $value = $options['value'];
+    if ($value === true || $value === 'on') $value=1;
+    else if ($value === false || $value === 'off') $value=0;
+
+    // if we have the device name, find the ID
+    if (isset($options['name'])) {
+      $options['name'] = strtolower($options['name']);
+      $devices = $this->getDevices();
+      foreach($devices as $device) {
+        if (strtolower($device->name) === $options['name']) {
+          $devId = $device->id;
+          break;
+        }
+      }
+    } else {
+      $devId = $options['id'];
+    }
+
+    $json = $this->post($this->uri.'/skill', [
+      "header" => [
+        "name" => $options['command'],
+        "namespace" => 'control',
+        "payloadVersion" => 1
+      ],
+      "payload" => [
+        "accessToken" => $this->accessToken,
+        "devId" => $devId,
+        "value" => $value
+      ]
+    ]);
+    return $json->header->code === "SUCCESS";
+  }
+
+  /**
+   * Get the state for a device
+   *
+   * @param   $option
+   *   @param {String} [$id] The device ID
+   *   @param {String} [$name] The device Name: either the ID or the Name of the device must be provided
+   *
+   * @return {Boolean|Number} TRUE (for "on"), FALSE (for "off"), or a value (e.g. a cover could return 3)
+   */
+  function getState($options) {
+    $hasName = isset($options['name']);
+    if ($hasName) $options['name'] = strtolower($options['name']);
+
+    $devices = $this->getDevices();
+    foreach($devices as $device) {
+      if (($hasName && strtolower($device->name) === $options['name']) || (!$hasName && $device->id === $options['id'])) {
+        return $device->data->state;
+      }
+    }
+  }
+}
+?>
